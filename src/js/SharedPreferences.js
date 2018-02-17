00514c317b79996939a3c17c1125a5c9acc9b247
Stefano Gargiulo <info@creativeprogramming.it>
2018-02-17 02:06:57 +0100

    Update SharedPreferences.js

--- src/js/SharedPreferences.js
+++ src/js/SharedPreferences.js
@@ -8,16 +8,16 @@ export function putString(key, string, successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, PLUGIN, 'putString', [key, string])
 }
 
-export function getString(key, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, PLUGIN, 'getString', [key])
+export function getString(key, defaultVal, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, PLUGIN, 'getString', [key, defaultVal])
 }
 
 export function putBoolean(key, string, successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, PLUGIN, 'putBoolean', [key, string])
 }
 
-export function getBoolean(key, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, PLUGIN, 'getBoolean', [key])
+export function getBoolean(key, defaultVal, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, PLUGIN, 'getBoolean', [key, defaultVal])
 }
 
 export function remove(key, successCallback, errorCallback){
